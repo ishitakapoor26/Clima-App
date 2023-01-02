4a01f6730773f7b407a9941413606ebfe7d79c95
Ishita Kapoor <ishitakapoor12612@gmail.com>
2023-01-02 22:25:43 +0530

    Complete application

---
+++ lib/services/location.dart
@@ -0,0 +1,23 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location  {
+  late double latitude;
+  late double longitude;
+
+
+  Future<void> getCurrentLocation() async {
+    try {
+      LocationPermission permission = await Geolocator.requestPermission();
+      if (permission != 'denied') {
+        Position position = await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.low);
+        print(position);
+        latitude=position.latitude;
+        longitude=position.longitude;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+}
